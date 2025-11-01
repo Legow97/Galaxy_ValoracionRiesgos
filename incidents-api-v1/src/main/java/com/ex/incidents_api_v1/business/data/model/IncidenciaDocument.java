a6e05568a2821d3da062c8df9438471605ef8a44
Legow97 <waldirlp1997@gmail.com>
2025-11-01 13:18:49 -0500

    Versión inicial automatización de valoración de riesgos

---
+++ incidents-api-v1/src/main/java/com/ex/incidents_api_v1/business/data/model/IncidenciaDocument.java
@@ -0,0 +1,14 @@
+package com.ex.incidents_api_v1.business.data.model;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "incidencias")
+@NoArgsConstructor
+@Data
+public class IncidenciaDocument {
+
+
+    }
